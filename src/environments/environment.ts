fed421c98a53efbefbb378ead7484b4bac00de45
Kanokpit Yowaratch <kanokpit.y@gmail.com>
2024-02-06 22:49:40 +0700

    Change server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     PORT: 4000,
-    SERVER: "https://json-server-product-master.vercel.app"
+    SERVER: "https://json-server-nu-blond.vercel.app"
 };
